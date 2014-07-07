24af63c6ad9002a8cb33ee79261d31a7f5300ca0
Nilesh Chakraborty <nilesh@nileshc.com>
2014-07-08 03:51:55 +0530

    Check if cache exists before reading it; do not count()
    count() forces early evaluation of RDD = performance penalty

--- distributed/src/main/scala/org/dbpedia/extraction/dump/extract/DistConfigLoader.scala
+++ distributed/src/main/scala/org/dbpedia/extraction/dump/extract/DistConfigLoader.scala
@@ -64,11 +64,11 @@ class DistConfigLoader(config: DistConfig, sparkContext: SparkContext)
     val cache = finder.file(date, "articles-rdd")
     val articlesRDD: RDD[WikiPage] = try
     {
+      if (!cache.exists)
+        throw new IOException("Cache file " + cache + " does not exist.")
       logger.info("Loading articles from cache file " + cache)
       val loaded = DistIOUtils.loadRDD(sparkContext, classOf[WikiPage], cache)
-      // count() throws org.apache.hadoop.mapred.InvalidInputException if file doesn't exist
-      val count = loaded.count()
-      logger.info(count + " WikiPages loaded from cache file " + cache)
+      logger.info("WikiPages loaded from cache file " + cache)
       loaded
     }
     catch
