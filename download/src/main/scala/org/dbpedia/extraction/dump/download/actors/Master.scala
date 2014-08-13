afe6d631f9ef52efb1a2b0c1b32ed4f1042bae00
Nilesh Chakraborty <nilesh@nileshc.com>
2014-08-13 19:08:48 +0530

    Implement Master and Worker actors

---
+++ download/src/main/scala/org/dbpedia/extraction/dump/download/actors/Master.scala
@@ -0,0 +1,226 @@
+package org.dbpedia.extraction.dump.download.actors
+
+import scala.concurrent.duration.{Deadline, FiniteDuration}
+import akka.actor._
+import akka.contrib.pattern.{DistributedPubSubMediator, DistributedPubSubExtension}
+import scala.collection.immutable.Queue
+import org.dbpedia.extraction.dump.download.actors.protocol.GeneralProtocol.ShutdownCluster
+import scala.Some
+import akka.contrib.pattern.DistributedPubSubMediator.Put
+import org.dbpedia.extraction.dump.download.actors.protocol.MasterWorkerProtocol
+import java.net.URL
+
+/**
+ * Master/driver node actor.
+ * TODO: Add documentation
+ *
+ * @param workTimeout Workers need to send download progress reports within this timeout
+ * @param mirrors List of wikipedia mirror URLs
+ * @param threadsPerMirror Number of simultaneous downloads per mirror
+ */
+class Master(workTimeout: FiniteDuration, mirrors: Seq[URL], threadsPerMirror: Int) extends Actor with ActorLogging
+{
+
+  import Master._
+  import MasterWorkerProtocol._
+  import context.dispatcher
+
+  def scheduler = context.system.scheduler
+
+  val mediator = DistributedPubSubExtension(context.system).mediator
+
+  mediator ! Put(self)
+
+  private var workers = Map[String, WorkerState]()
+  private var pendingDownloads = Queue[DownloadJob]()
+  private var downloadIds = Set[String]()
+  private var mirrorsInUse = (mirrors zip Seq.fill(mirrors.size)(0)).toMap // Mapping mirror URL to number of simultaneous downloads
+
+  val cleanupTask = scheduler.schedule(workTimeout / 2, workTimeout / 2,
+                                       self, CleanupTick)
+
+
+  override def postStop(): Unit = cleanupTask.cancel()
+
+  def receive =
+  {
+    case ShutdownCluster =>
+      if (pendingDownloads.isEmpty)
+      {
+        if (workers.isEmpty)
+        {
+          log.info("Stopping master!")
+          mediator ! DistributedPubSubMediator.Publish(General, ShutdownCluster)
+          self ! PoisonPill
+          context.stop(self)
+          context.system.shutdown()
+        } else
+        {
+          workers.foreach
+          {
+            case (_, WorkerState(ref, Idle)) => ref ! ShutdownCluster
+            case _ =>
+          }
+          log.info("Some workers still busy! Cannot stop master yet!")
+          context.system.scheduler.scheduleOnce(workTimeout, self, ShutdownCluster)
+        }
+      } else
+      {
+        log.info("Some work pending! Cannot stop master yet!")
+        context.system.scheduler.scheduleOnce(workTimeout, self, ShutdownCluster)
+      }
+
+    case RemoveWorker(workerId) =>
+      workers -= workerId
+
+    case ProgressReport(workerId: String, progress: Long) =>
+      log.info("Heard from worker {}: {} ", workerId, progress)
+      workers.get(workerId) match
+      {
+        case Some(s@WorkerState(_, Busy(downloadJob, deadline))) =>
+          workers -= workerId
+          workers += (workerId -> WorkerState(sender, status = Busy(downloadJob, Deadline.now + workTimeout)))
+
+        case x => log.info("NOTHING RECEIVNEKJNF" + x)
+      }
+
+    case RegisterWorker(workerId) =>
+      if (workers.contains(workerId))
+      {
+        workers += (workerId -> workers(workerId).copy(ref = sender))
+      } else
+      {
+        log.info("Worker registered: {}", workerId)
+        workers += (workerId -> WorkerState(sender, status = Idle))
+        if (pendingDownloads.nonEmpty)
+          sender ! DownloadIsReady
+      }
+
+    case WorkerRequestsDownload(workerId) =>
+      if (pendingDownloads.nonEmpty)
+      {
+        workers.get(workerId) match
+        {
+          case Some(s@WorkerState(_, Idle)) =>
+            getFreeMirror foreach
+            {
+              case url => // We have a free mirror!
+                val (downloadJob, rest) = pendingDownloads.dequeue
+                pendingDownloads = rest
+                val downloadWithMirror = MirroredDownloadJob(url, downloadJob)
+                log.info("Giving worker {} a download job {}", workerId, downloadWithMirror)
+                // TODO store in Eventsourced
+                sender ! downloadWithMirror
+                mirrorsInUse += (url -> (mirrorsInUse(url) + 1))
+                workers += (workerId -> s.copy(status = Busy(downloadWithMirror, Deadline.now + workTimeout)))
+            }
+          case _ =>
+        }
+      }
+
+    case DownloadIsDone(workerId, downloadId, outputPath, totalBytes) =>
+      workers.get(workerId) match
+      {
+        case Some(s@WorkerState(_, Busy(downloadJob, _))) if downloadJob.job.downloadId == downloadId =>
+          log.info("Download is done: {} => {},{} by worker {}", downloadJob, outputPath, totalBytes, workerId)
+          // TODO store in Eventsourced
+
+          val mirror = downloadJob.baseUrl
+          mirrorsInUse += (mirror -> (mirrorsInUse(mirror) - 1))
+          workers += (workerId -> s.copy(status = Idle))
+
+          mediator ! DistributedPubSubMediator.Publish(ResultsTopic, DownloadResult(downloadId, outputPath, totalBytes))
+
+          sender ! MasterWorkerProtocol.Ack(downloadId)
+        case _ =>
+          if (downloadIds.contains(downloadId))
+          {
+            // previous Ack was lost, confirm again that this is done
+            sender ! MasterWorkerProtocol.Ack(downloadId)
+          }
+      }
+
+    case DownloadFailed(workerId, downloadId) =>
+      workers.get(workerId) match
+      {
+        case Some(s@WorkerState(_, Busy(downloadJob, _))) if downloadJob.job.downloadId == downloadId =>
+          log.info("Download failed: {}", downloadJob)
+          // TODO store in Eventsourced
+          val mirror = downloadJob.baseUrl
+          mirrorsInUse += (mirror -> (mirrorsInUse(mirror) - 1))
+          workers += (workerId -> s.copy(status = Idle))
+
+          pendingDownloads = pendingDownloads enqueue downloadJob.job
+          notifyWorkers()
+        case _ =>
+      }
+
+    case job: DownloadJob =>
+      // idempotent
+      if (downloadIds.contains(job.downloadId))
+      {
+        sender ! Master.Ack(job.downloadId)
+      } else
+      {
+        log.info("Accepted download: {}", job)
+        // TODO store in Eventsourced
+        pendingDownloads = pendingDownloads enqueue job
+        downloadIds += job.downloadId
+        sender ! Master.Ack(job.downloadId)
+        notifyWorkers()
+      }
+
+    case CleanupTick =>
+      for ((workerId, s@WorkerState(_, Busy(downloadJob, timeout))) <- workers)
+      {
+        if (timeout.isOverdue)
+        {
+          log.info("Download timed out: {}", downloadJob)
+          // TODO store in Eventsourced
+          workers -= workerId
+          pendingDownloads = pendingDownloads enqueue downloadJob.job
+          notifyWorkers()
+        }
+      }
+  }
+
+  def getFreeMirror: Option[URL] =
+    mirrorsInUse.find(_._2 < threadsPerMirror) match
+    {
+      case Some((url, _)) => Some(url)
+      case _ => None
+    }
+
+  def notifyWorkers(): Unit =
+    if (pendingDownloads.nonEmpty)
+    {
+      // could pick a few random instead of all
+      workers.foreach
+      {
+        case (_, WorkerState(ref, Idle)) => ref ! DownloadIsReady
+        case _ => // busy
+      }
+    }
+
+  // TODO cleanup old workers
+  // TODO cleanup old downloadIds
+}
+
+object Master
+{
+  val ResultsTopic = "results"
+  val General = "general"
+
+  def props(workTimeout: FiniteDuration, mirrors: Seq[URL], threadsPerMirror: Int): Props =
+    Props(classOf[Master], workTimeout, mirrors, threadsPerMirror)
+
+  case class Ack(downloadId: String)
+
+  private sealed trait WorkerStatus
+  private case object Idle extends WorkerStatus
+  private case class Busy(job: MirroredDownloadJob, deadline: Deadline) extends WorkerStatus
+  private case class WorkerState(ref: ActorRef, status: WorkerStatus)
+
+  private case object CleanupTick
+
+}
---
+++ download/src/main/scala/org/dbpedia/extraction/dump/download/actors/Worker.scala
@@ -0,0 +1,141 @@
+package org.dbpedia.extraction.dump.download.actors
+
+import akka.actor._
+import scala.concurrent.duration._
+import java.util.UUID
+import akka.actor.SupervisorStrategy.{Stop, Restart}
+import akka.contrib.pattern.ClusterClient.SendToAll
+import scala.Some
+import akka.actor.OneForOneStrategy
+import org.dbpedia.extraction.dump.download.actors.protocol.{WorkerProgressProtocol, MasterWorkerProtocol, GeneralProtocol}
+import GeneralProtocol.ShutdownCluster
+import org.dbpedia.extraction.dump.download.actors.Worker.DownloadComplete
+
+/**
+ * Worker actor that runs on each worker node.
+ *
+ * TODO: Add documentation
+ */
+class Worker(clusterClient: ActorRef, downloadRunnerProps: Props, registerInterval: FiniteDuration)
+  extends Actor with ActorLogging
+{
+
+  import MasterWorkerProtocol._
+  import WorkerProgressProtocol._
+  import context.dispatcher
+
+  def scheduler = context.system.scheduler
+
+  val workerId = UUID.randomUUID().toString
+
+  val registerTask = context.system.scheduler.schedule(0.seconds, registerInterval, clusterClient,
+                                                       SendToAll("/user/master/active", RegisterWorker(workerId)))
+
+  val downloadRunner = context.watch(context.actorOf(downloadRunnerProps, "runner"))
+
+  var currentDownloadId: Option[String] = None
+
+  private var totalBytes = 0l
+  private var currentBytes = 0l
+  private var progressDelays = 0
+  private val MaxProgressDelays = 5
+
+  def workId: String = currentDownloadId match
+  {
+    case Some(workId) => workId
+    case None => throw new IllegalStateException("Not working")
+  }
+
+  override def supervisorStrategy =
+    OneForOneStrategy()
+    {
+      case _: ActorInitializationException => Stop
+      case _: DeathPactException => Stop
+      case _: Exception =>
+        currentDownloadId foreach (workId => sendToMaster(DownloadFailed(workerId, workId)))
+        context.become(idle)
+        if (registerTask.isCancelled) Stop else Restart
+    }
+
+  override def postStop(): Unit = registerTask.cancel()
+
+  def receive = idle
+
+  def idle: Receive =
+  {
+    case ShutdownCluster =>
+      sendToMaster(RemoveWorker(workerId))
+      registerTask.cancel()
+      context.stop(downloadRunner)
+      context.stop(self)
+      context.system.shutdown()
+
+    case DownloadIsReady =>
+      sendToMaster(WorkerRequestsDownload(workerId))
+
+    case job @ MirroredDownloadJob(_, DownloadJob(downloadId, _)) =>
+      log.info("Got download job: {}", job)
+      currentDownloadId = Some(downloadId)
+      downloadRunner ! job
+      context.become(working)
+  }
+
+  def working: Receive =
+  {
+    case ProgressStart(total) =>
+      if(totalBytes == 0) totalBytes = total
+
+    case Progress(bytes) =>
+      if(bytes > currentBytes) currentBytes = bytes else progressDelays += 1
+      sendToMaster(ProgressReport(workerId, bytes))
+      if(progressDelays > MaxProgressDelays)
+      {
+        val delay = progressDelays * downloadRunnerProps.args(0).asInstanceOf[FiniteDuration].toSeconds
+        throw new Exception(s"Download progress has stagnated. No update occurred in $delay seconds!")
+      }
+
+    case DownloadComplete(output, bytes) =>
+      log.info("Download is complete. Output file: {}. Total bytes: {}", output, bytes)
+      sendToMaster(DownloadIsDone(workerId, workId, output, bytes))
+      context.setReceiveTimeout(10.seconds)
+      context.become(waitForDownloadIsDoneAck(output, bytes))
+
+    case ShutdownCluster =>
+      log.info("Yikes. Master told me to shutdown, while I'm downloading.")
+
+    case _: MirroredDownloadJob =>
+      log.info("Yikes. Master told me to do download, while I'm downloading.")
+  }
+
+  def waitForDownloadIsDoneAck(outputFilePath: String, bytes: Long): Receive =
+  {
+    case Ack(id) if id == workId =>
+      sendToMaster(WorkerRequestsDownload(workerId))
+      context.setReceiveTimeout(Duration.Undefined)
+      context.become(idle)
+    case ReceiveTimeout =>
+      log.info("No ack from master, retrying")
+      sendToMaster(DownloadIsDone(workerId, workId, outputFilePath, bytes))
+  }
+
+  override def unhandled(message: Any): Unit = message match
+  {
+    case Terminated(`downloadRunner`) => context.stop(self)
+    case DownloadIsReady =>
+    case _ => super.unhandled(message)
+  }
+
+  def sendToMaster(msg: Any): Unit =
+  {
+    clusterClient ! SendToAll("/user/master/active", msg)
+  }
+}
+
+object Worker
+{
+  def props(clusterClient: ActorRef, downloadRunnerProps: Props, registerInterval: FiniteDuration = 10.seconds): Props =
+    Props(classOf[Worker], clusterClient, downloadRunnerProps, registerInterval)
+
+  case class DownloadComplete(outputFilePath: String, bytes: Long)
+
+}
