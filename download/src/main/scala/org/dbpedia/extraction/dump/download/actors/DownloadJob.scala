8bb1d6980688347fb3a5663a4340672594569034
Nilesh Chakraborty <nilesh@nileshc.com>
2014-08-13 19:00:44 +0530

    Add DownloadJob messages

---
+++ download/src/main/scala/org/dbpedia/extraction/dump/download/actors/DownloadJob.scala
@@ -0,0 +1,43 @@
+package org.dbpedia.extraction.dump.download.actors
+
+import java.net.URL
+import java.io.File
+import org.dbpedia.extraction.util.Language
+import org.apache.hadoop.fs.Path
+
+/**
+ * Download job wrapped along with the mirror to use for downloading.
+ * This contains all the information needed by DownloadJobRunner to perform the job.
+ *
+ * @param baseUrl URL of the mirror to download from
+ * @param job download job
+ */
+case class MirroredDownloadJob(baseUrl: URL, job: DownloadJob)
+
+/**
+ * Download job used by the actor framework.
+ *
+ * @param downloadId Unique job ID
+ * @param info URL information
+ */
+case class DownloadJob(downloadId: String, info: DumpFileInfo)
+
+/**
+ * Download information for single wiki dump file.
+ *
+ * @param baseDir Base directory on Hadoop file system (HDFS for distributed downloads)
+ * @param wikiSuffix Wiki name suffix (eg. wiki)
+ * @param language Wiki language
+ * @param date YYYYMMDD date string
+ * @param fileName File name to save the URL to
+ */
+case class DumpFileInfo(baseDir: Path, wikiSuffix: String, language: Language, date: String, fileName: String)
+
+/**
+ * Download job used by the actor framework.
+ *
+ * @param downloadId Unique job ID
+ */
+case class DownloadResult(downloadId: String, outputPath: String, bytes: Long)
+
+
