19fef98cd1f16c1d9d169e981f9bb4f26da88a85
Nilesh Chakraborty <nilesh@nileshc.com>
2014-08-17 05:48:04 +0530

    Add finish/shutdown logic to DownloadClient

--- download/src/main/scala/org/dbpedia/extraction/dump/download/actors/DownloadClient.scala
+++ download/src/main/scala/org/dbpedia/extraction/dump/download/actors/DownloadClient.scala
@@ -4,10 +4,11 @@ import akka.actor.{ActorLogging, Actor}
 import java.util.UUID
 import scala.concurrent.duration._
 import akka.pattern._
-import akka.contrib.pattern.DistributedPubSubExtension
+import akka.contrib.pattern.{DistributedPubSubMediator, DistributedPubSubExtension}
 import akka.contrib.pattern.DistributedPubSubMediator.Send
-import org.dbpedia.extraction.dump.download.actors.message.GeneralMessage.ShutdownCluster
+import org.dbpedia.extraction.dump.download.actors.message.GeneralMessage.{MasterQueueEmpty, ShutdownCluster}
 import akka.util.Timeout
+import org.dbpedia.extraction.dump.download.actors.message.{DumpFile, DownloadJob}
 
 /**
  * A client actor used to submit download jobs to the master.
@@ -15,6 +16,7 @@ import akka.util.Timeout
 class DownloadClient extends Actor with ActorLogging
 {
 
+  import DownloadClient._
   import context.dispatcher
 
   def scheduler = context.system.scheduler
@@ -22,15 +24,26 @@ class DownloadClient extends Actor with ActorLogging
   def nextDownloadId(): String = UUID.randomUUID().toString
 
   val mediator = DistributedPubSubExtension(context.system).mediator
+  mediator ! DistributedPubSubMediator.Subscribe(Master.General, self)
 
   implicit val timeout = Timeout(10.seconds)
 
+  var canShutDownCluster = false
+
   def receive =
   {
+    case Finished =>
+      // send this when no more DumpFiles are to be added - ready for shutdown
+      canShutDownCluster = true
+
+    case MasterQueueEmpty =>
+      if (canShutDownCluster) self ! ShutdownCluster
+
     case ShutdownCluster =>
       mediator ! Send("/user/master/active", ShutdownCluster, localAffinity = false)
       context.stop(self)
       context.system.shutdown()
+      context.become(shuttingDown)
 
     case file: DumpFile =>
       self ! DownloadJob(nextDownloadId(), file)
@@ -47,4 +60,14 @@ class DownloadClient extends Actor with ActorLogging
             scheduler.scheduleOnce(3.seconds, self, job)
         }
   }
+
+  def shuttingDown: Receive =
+  {
+    case _ => // ignore all messages, shutting down cluster.
+  }
+}
+
+object DownloadClient
+{
+  case object Finished
 }
