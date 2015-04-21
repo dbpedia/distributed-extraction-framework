784bbb4fdb6213d7c6b6104b7f3dadd473d2ed55
gone-phishing <riteshoneinamillion@gmail.com>
2015-04-21 18:58:01 +0530

    Fixed FunSuite for scalatest issue (FunSuite needs to be a trait and so we use FunSuiteLike)

--- extraction/src/test/scala/org/dbpedia/extraction/spark/io/QuadSeqWritableTest.scala
+++ extraction/src/test/scala/org/dbpedia/extraction/spark/io/QuadSeqWritableTest.scala
@@ -1,30 +1,32 @@
 package org.dbpedia.extraction.spark.io
 
+
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
-import org.scalatest.FunSuite
 import org.dbpedia.extraction.destinations.Quad
 import scala.util.Random
 import org.junit.Assert._
+import org.scalatest.fixture.FunSuiteLike
 
 @RunWith(classOf[JUnitRunner])
-class QuadSeqWritableTest extends WritableTest[QuadSeqWritable] with FunSuite
+class QuadSeqWritableTest extends WritableTest[QuadSeqWritable] with FunSuiteLike
 {
   test("Verify that serialization-deserialization works properly")
   {
     // Create random List[Quad]
     val sampleQuads = for (i <- (0 until 100).toList) yield new Quad(Random.nextString(10),
-                                                                     Random.nextString(10),
-                                                                     Random.nextString(10),
-                                                                     Random.nextString(10),
-                                                                     Random.nextString(10),
-                                                                     Random.nextString(10),
-                                                                     Random.nextString(10))
+      Random.nextString(10),
+      Random.nextString(10),
+      Random.nextString(10),
+      Random.nextString(10),
+      Random.nextString(10),
+      Random.nextString(10))
 
     val writable1 = new QuadSeqWritable(sampleQuads)
     val writable2 = new QuadSeqWritable()
 
     performReadWriteRoundTrip(writable1, writable2)
-    assertEquals(writable1.get, writable2.get)
+    assertEquals(writable1.get.toString, writable2.get.toString)
+
   }
 }
